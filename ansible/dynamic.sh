53ddf0ca96918e7de9855665db1dfd5514bf1bb2
user <user@localhost.localdomain>
2020-11-12 21:04:57 +0300

    ansible-1

--- ansible/dynamic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-yc_instances_app=($(yc compute instance list | grep app | awk -F\| '{print $3 $6}'))
-yc_instances_db=($(yc compute instance list | grep db |  awk -F\| '{print $3 $6}'))
